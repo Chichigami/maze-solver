c3a01f88bd8caca2ca70598ab0a5b8530d70d847
Chichigami <g.feng.work@gmail.com>
2024-07-18 16:12:26 -0400

    redone cell.draw_move() method

--- cell.py
+++ cell.py
@@ -5,7 +5,6 @@ class Cell:
     def __init__(self, window: Window):
         self._win = window
 
-
     def draw(self, x_top_left, y_top_left, x_bottom_right, y_bottom_right, top_wall = True, right_wall = True, bottom_wall = True, left_wall = True):
         self.has_top_wall = top_wall
         self.has_right_wall = right_wall
@@ -25,10 +24,13 @@ class Cell:
             self._win.draw_line(Line(self.__bottom_right_point, self.__bottom_left_point))
         if self.has_left_wall:
             self._win.draw_line(Line(self.__top_left_point, self.__bottom_left_point))
+        
+        self.middle_of_cell = Point((x_top_left + x_bottom_right) / 2,  #this is for draw_move()
+                                    (y_top_left + y_bottom_right) / 2)
 
     def draw_move(self, to_cell, undo = False):
         path = Line(self.middle_of_cell, to_cell.middle_of_cell)
+        color = "red"
         if undo:
-            self.__win.draw_line(path, "gray")
-        else:
-            self.__win.draw_line(path, "red")
+            color = "gray"
+        self._win.draw_line(path, color)
--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from cell import *
 def main():
     win = Window(800, 600)
 
-    cell_1 = Cell(win)
-    cell_1.draw(10, 10, 20, 20, False, False, True, True)
-    cell_2 = Cell(win)
-    cell_2.draw(20, 10, 30, 20, True, True, False, False)
+    # cell_1 = Cell(win)
+    # cell_1.draw(10, 10, 20, 20, False, False, True, True)
+    # cell_2 = Cell(win)
+    # cell_2.draw(20, 10, 30, 20, True, True, False, False)
+    # cell_1.draw_move(cell_2)
 
     win.wait_for_close()
 
